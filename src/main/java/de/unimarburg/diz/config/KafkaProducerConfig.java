e3f0eecc761214a624b10a458ebc85dfecf6053b
Jakub Lidke <jakub.lidke@uni-marburg.de>
2024-11-05 10:35:12 +0100

    fix: auto config kafka producer

--- src/main/java/de/unimarburg/diz/config/KafkaProducerConfig.java
+++ src/main/java/de/unimarburg/diz/config/KafkaProducerConfig.java
@@ -1,42 +1,26 @@
 /* GNU AFFERO GENERAL PUBLIC LICENSE  Version 3 (C)2024 Datenintegrationszentrum Fachbereich Medizin Philipps Universität Marburg */
 package de.unimarburg.diz.config;
 
-import java.util.HashMap;
-import java.util.Map;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaProducerConfig {
 
-  @Value(value = "${spring.kafka.bootstrap-servers}")
-  private String bootstrapAddress;
+  private final ProducerFactory producerFactory;
 
-  public Map<String, Object> configProps() {
-    Map<String, Object> configProps = new HashMap<>();
-    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    configProps.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, 5242880);
-    configProps.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 5242880);
-
-    return configProps;
-  }
-
-  public ProducerFactory<String, String> producerFactory() {
-    return new DefaultKafkaProducerFactory<>(configProps());
+  @Autowired
+  public KafkaProducerConfig(ProducerFactory producerFactory) {
+    this.producerFactory = producerFactory;
   }
 
   @Bean
   public KafkaTemplate<String, String> kafkaTemplate() {
     final KafkaTemplate<String, String> stringStringKafkaTemplate =
-        new KafkaTemplate<>(producerFactory());
+        new KafkaTemplate<>(producerFactory);
 
     return stringStringKafkaTemplate;
   }
